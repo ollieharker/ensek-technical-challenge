a8b7a97d32e990f67e3db922da2bcf27c23ff91c
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 17:35:07 +0100

    Removed nullables from entities.

--- Ensek.TechnicalTest.Db/Models/Account.cs
+++ Ensek.TechnicalTest.Db/Models/Account.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
 
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
     }
 }
