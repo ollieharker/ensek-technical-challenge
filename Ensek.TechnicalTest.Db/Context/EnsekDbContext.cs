b43c7de6e2c5319c37ea746231e54d70ef1cac7d
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 09:47:43 +0100

    Added database objects.

---
+++ Ensek.TechnicalTest.Db/Context/EnsekDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Ensek.TechnicalTest.Db.Models;
+using Ensek.TechnicalTest.Db.Seeding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Ensek.TechnicalTest.Db.Context
+{
+    public class EnsekDbContext : DbContext
+    {
+        public EnsekDbContext()
+                : base()
+        {
+        }
+
+        public EnsekDbContext(DbContextOptions<EnsekDbContext> options)
+                : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .HasKey(x => x.Id);
+
+            AccountEntitySeeder.SeedData(modelBuilder);
+
+            modelBuilder.Entity<MeterReading>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<MeterReading>()
+                .HasOne(x => x.Account);
+        }
+
+        public DbSet<MeterReading> MeterReadings { get; set; }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
