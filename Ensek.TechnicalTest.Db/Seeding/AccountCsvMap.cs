995d297e2bc34a9e5e77186a6754b9109147aa8c
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 09:48:32 +0100

    Added data seeding via csv file.

---
+++ Ensek.TechnicalTest.Db/Seeding/AccountCsvMap.cs
@@ -0,0 +1,16 @@
+﻿
+using CsvHelper.Configuration;
+using Ensek.TechnicalTest.Db.Models;
+
+namespace Ensek.TechnicalTest.Db.Seeding
+{
+	public class AccountCsvMap : ClassMap<Account>
+	{
+		public AccountCsvMap()
+		{
+			Map(m => m.Id).Name("AccountId");
+			Map(m => m.FirstName).Name("FirstName");
+			Map(m => m.LastName).Name("LastName");
+		}
+	}
+}
---
+++ Ensek.TechnicalTest.Db/Seeding/AccountEntitySeeder.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper;
+using Ensek.TechnicalTest.Db.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace Ensek.TechnicalTest.Db.Seeding
+{
+	internal static class AccountEntitySeeder
+	{
+		/// <summary>
+		/// Seed the data for the <see cref="Account"/> entity.
+		/// </summary>
+		/// <param name="modelBuilder">The model builder to seed the data with.</param>
+		public static void SeedData(ModelBuilder modelBuilder)
+		{
+			using(var reader = new StreamReader($"{AppDomain.CurrentDomain.BaseDirectory}/Seeding/Data/Test_Accounts.csv"))
+			using(var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+			{
+				csvReader.Context.RegisterClassMap<AccountCsvMap>();
+
+				var accounts = csvReader.GetRecords<Account>();
+				modelBuilder.Entity<Account>()
+					.HasData(accounts);
+			}
+		}
+	}
+}
