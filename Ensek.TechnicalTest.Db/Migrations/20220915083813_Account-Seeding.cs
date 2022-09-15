241c975f7cd686e2a57c4b30fbf8fdd61a6ae4f2
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 09:48:52 +0100

    Added migrations for database project.

---
+++ Ensek.TechnicalTest.Db/Migrations/20220915083813_Account-Seeding.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ensek.TechnicalTest.Db.Migrations
+{
+    public partial class AccountSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1234, "Freya", "Test" },
+                    { 1239, "Noddy", "Test" },
+                    { 1240, "Archie", "Test" },
+                    { 1241, "Lara", "Test" },
+                    { 1242, "Tim", "Test" },
+                    { 1243, "Graham", "Test" },
+                    { 1244, "Tony", "Test" },
+                    { 1245, "Neville", "Test" },
+                    { 1246, "Jo", "Test" },
+                    { 1247, "Jim", "Test" },
+                    { 1248, "Pam", "Test" },
+                    { 2233, "Barry", "Test" },
+                    { 2344, "Tommy", "Test" },
+                    { 2345, "Jerry", "Test" },
+                    { 2346, "Ollie", "Test" },
+                    { 2347, "Tara", "Test" },
+                    { 2348, "Tammy", "Test" },
+                    { 2349, "Simon", "Test" },
+                    { 2350, "Colin", "Test" },
+                    { 2351, "Gladys", "Test" },
+                    { 2352, "Greg", "Test" },
+                    { 2353, "Tony", "Test" },
+                    { 2355, "Arthur", "Test" },
+                    { 2356, "Craig", "Test" },
+                    { 4534, "JOSH", "TEST" },
+                    { 6776, "Laura", "Test" },
+                    { 8766, "Sally", "Test" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1234);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1239);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1240);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1241);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1242);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1243);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1244);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1245);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1246);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1247);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1248);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2233);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2344);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2345);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2346);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2347);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2348);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2349);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2350);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2351);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2352);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2353);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2355);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 2356);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 4534);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 6776);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 8766);
+        }
+    }
+}
