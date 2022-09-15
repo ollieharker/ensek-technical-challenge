0f73651033a1df37b149d7b0913204e4e7c27aa0
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 17:35:37 +0100

    Added/Updated extension classes for use in program.cs.

---
+++ Ensek.TechnicalTest.Api/Ensek.TechnicalTest.Api/Extensions/NewMeterReadDtoExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Ensek.TechnicalTest.BusinessLogic.Dto.MeterReading;
+using Ensek.TechnicalTest.Db.Models;
+
+namespace Ensek.TechnicalTest.Api.Extensions
+{
+	public static class NewMeterReadingDtoExtensions
+	{
+		public static MeterReading ToMeterReading(this NewMeterReadingDto dto)
+		{
+			return new MeterReading()
+			{
+				DateTime = dto.DateTime,
+				AccountId = dto.AccountId,
+				Value = dto.Value
+			};
+		}
+	}
+}
