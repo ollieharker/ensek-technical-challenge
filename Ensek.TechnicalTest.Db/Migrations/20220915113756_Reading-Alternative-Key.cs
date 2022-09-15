f1e38ac737d49bb12484e2a8b9c4106458fe4452
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 17:26:41 +0100

    Added migration for alternative key on account id, datetime and value.

---
+++ Ensek.TechnicalTest.Db/Migrations/20220915113756_Reading-Alternative-Key.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ensek.TechnicalTest.Db.Migrations
+{
+    public partial class ReadingAlternativeKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MeterReadings_Accounts_AccountId",
+                table: "MeterReadings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MeterReadings_AccountId",
+                table: "MeterReadings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "MeterReadings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_MeterReadings_AccountId_DateTime_Value",
+                table: "MeterReadings",
+                columns: new[] { "AccountId", "DateTime", "Value" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MeterReadings_Accounts_AccountId",
+                table: "MeterReadings",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MeterReadings_Accounts_AccountId",
+                table: "MeterReadings");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_MeterReadings_AccountId_DateTime_Value",
+                table: "MeterReadings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountId",
+                table: "MeterReadings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeterReadings_AccountId",
+                table: "MeterReadings",
+                column: "AccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MeterReadings_Accounts_AccountId",
+                table: "MeterReadings",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+        }
+    }
+}
