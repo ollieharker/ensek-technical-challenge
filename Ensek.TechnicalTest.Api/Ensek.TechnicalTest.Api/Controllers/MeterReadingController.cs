e8b209e2f4bf666e72dccb0e040d1e8d1629c305
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 17:36:43 +0100

    Added controller and CSV map.

---
+++ Ensek.TechnicalTest.Api/Ensek.TechnicalTest.Api/Controllers/MeterReadingController.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Ensek.TechnicalTest.Api.Dto;
+using Ensek.TechnicalTest.Api.Services.MeterReads;
+using Ensek.TechnicalTest.BusinessLogic.Dto.MeterReading;
+using Ensek.TechnicalTest.BusinessLogic.Validation;
+using Ensek.TechnicalTest.Db.Context;
+using Ensek.TechnicalTest.Db.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Ensek.TechnicalTest.Api.Controllers
+{
+    [ApiController]
+    public class MeterReadingController
+    {
+        private readonly EnsekDbContext ensekDbContext;
+        private readonly IMeterReadCsvUploadService meterReadUploadService;
+
+        public MeterReadingController(EnsekDbContext ensekDbContext, IMeterReadCsvUploadService meterReadUploadService)
+        {
+            this.ensekDbContext = ensekDbContext;
+            this.meterReadUploadService = meterReadUploadService;
+        }
+
+        [HttpPost]
+        [Route("/meter-reading-uploads")]
+        public MeterReadingUploadResult UploadMeterReads([Required]IFormFile csvFile)
+        {
+            return this.meterReadUploadService.UploadMeterReadsFromStream(csvFile.OpenReadStream());
+		}
+	}
+}
---
+++ Ensek.TechnicalTest.Api/Ensek.TechnicalTest.Api/Services/MeterReads/MeterReadingCsvMap.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper.Configuration;
+using Ensek.TechnicalTest.BusinessLogic.Dto.MeterReading;
+
+namespace Ensek.TechnicalTest.Api.Services.MeterReads
+{
+	public class MeterReadingCsvMap : ClassMap<NewMeterReadingDto>
+	{
+		public MeterReadingCsvMap()
+		{
+			this.Map(model => model.AccountId).Name("AccountId");
+			this.Map(model => model.DateTime).Name("MeterReadingDateTime");
+			this.Map(model => model.Value).Name("MeterReadValue");
+		}
+	}
+}
