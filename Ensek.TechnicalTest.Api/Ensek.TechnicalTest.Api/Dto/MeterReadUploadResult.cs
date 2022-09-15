bdb0800a64554399dc4d37073bc58ad4be51f3eb
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-15 10:00:52 +0100

    Added initial meter read controller.

---
+++ Ensek.TechnicalTest.Api/Ensek.TechnicalTest.Api/Dto/MeterReadUploadResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Ensek.TechnicalTest.Api.Dto
+{
+    public record MeterReadUploadResult
+    {
+        public int TotalUploaded { get; }
+
+        public int UploadsSuccessful { get; }
+
+        public int UploadsFailed { get; set; }
+    }
+}
