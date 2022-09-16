88f8b0a6629a2f678a8270481bf39569186e3eb0
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-16 11:15:09 +0100

    Added unit tests for the mete read upload.

---
+++ Ensek.TechnicalTest.Api/Ensek.TechnicalTest.Api.Tests/Dto/MeterReadUploadResultTests.cs
@@ -0,0 +1,22 @@
+﻿using Ensek.TechnicalTest.Api.Dto;
+
+namespace Ensek.TechnicalTest.Api.Tests.Dto
+{
+	public class MeterReadUploadResultTests
+	{
+		public static IEnumerable<object[]> MeterReadUploadTestCases()
+		{
+			yield return new object[] { new MeterReadingUploadResult() { UploadsSuccessful = 3, UploadsFailedToParse = 2, UploadsFailedToSave = 0 }, 5 };
+			yield return new object[] { new MeterReadingUploadResult() { UploadsSuccessful = 0, UploadsFailedToParse = 23, UploadsFailedToSave = 3 }, 26 };
+			yield return new object[] { new MeterReadingUploadResult() { UploadsSuccessful = 0, UploadsFailedToParse = 0, UploadsFailedToSave = 0 }, 0 };
+		}
+
+
+		[Theory]
+		[MemberData(nameof(MeterReadUploadTestCases))]
+		public void GivenSuccessAndFailValuesCorrectTotalUploaded(MeterReadingUploadResult uploadResult, int expectedUploadedCount)
+		{
+			Assert.Equal(expectedUploadedCount, uploadResult.TotalUploaded);
+		}
+	}
+}
---
+++ Ensek.TechnicalTest.Api/Ensek.TechnicalTest.Api.Tests/Extensions/NewMeterReadDtoExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using CsvHelper;
+using Ensek.TechnicalTest.Api.Extensions;
+using Ensek.TechnicalTest.BusinessLogic.Dto.MeterReading;
+using Ensek.TechnicalTest.Db.Models;
+using System.Globalization;
+
+namespace Ensek.TechnicalTest.Api.Tests.Extensions
+{
+	public class NewMeterReadDtoExtensionsTests
+	{
+		public static IEnumerable<object[]> NewMeterReadingDtoTestData()
+		{
+			yield return new object[] { new MeterReading() { DateTime = DateTimeOffset.UtcNow, AccountId = 1234, Value = 632}};
+			yield return new object[] { new MeterReading() { DateTime = DateTimeOffset.UtcNow.AddDays(-5), AccountId = 4321, Value = 1000 } };
+			yield return new object[] { new MeterReading() { DateTime = DateTimeOffset.UtcNow.AddDays(365), AccountId = 1, Value = 7 } };
+		}
+
+		[Theory]
+		[MemberData(nameof(NewMeterReadingDtoTestData))]
+		public void ToMeterReadingGivenDtoAssignesDateTime(NewMeterReadingDto newMeterReadingDto)
+		{
+			var meterReading = newMeterReadingDto.ToMeterReading();
+
+			Assert.Equal(newMeterReadingDto.DateTime, meterReading.DateTime);
+		}
+
+		[Theory]
+		[MemberData(nameof(NewMeterReadingDtoTestData))]
+		public void ToMeterReadingGivenDtoAssignesAccountId(NewMeterReadingDto newMeterReadingDto)
+		{
+			var meterReading = newMeterReadingDto.ToMeterReading();
+
+			Assert.Equal(newMeterReadingDto.AccountId, meterReading.AccountId);
+		}
+
+		[Theory]
+		[MemberData(nameof(NewMeterReadingDtoTestData))]
+		public void ToMeterReadingGivenDtoAssignesValue(NewMeterReadingDto newMeterReadingDto)
+		{
+			var meterReading = newMeterReadingDto.ToMeterReading();
+
+			Assert.Equal(newMeterReadingDto.Value, meterReading.Value);
+		}
+
+		[Fact]
+		public void ToMeterReadingGivenDtoIdSetToDefault()
+		{
+			var meterReadingDto = new NewMeterReadingDto();
+
+			var meterReading = meterReadingDto.ToMeterReading();
+
+			Assert.Equal(default(int), meterReading.Id);
+		}
+
+		[Fact]
+		public void ToMeterReadingGivenDtoAccountSetToNull()
+		{
+			var meterReadingDto = new NewMeterReadingDto();
+
+			var meterReading = meterReadingDto.ToMeterReading();
+
+			Assert.Null(meterReading.Account);
+		}
+	}
+}
