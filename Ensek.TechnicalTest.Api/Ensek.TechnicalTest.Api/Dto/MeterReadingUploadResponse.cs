0fa432b1ab2f5d1919fc940b371202d4e0ea36c9
Ollie Harker <oliver.harker2@btinternet.com>
2022-09-18 11:43:00 +0100

    Added meter upload response to provide error messages from api.

---
+++ Ensek.TechnicalTest.Api/Ensek.TechnicalTest.Api/Dto/MeterReadingUploadResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Ensek.TechnicalTest.Api.Dto
+{
+    public class MeterReadingUploadResponse
+    {
+        public bool Success {get; set;}
+        public string Error { get; set; }
+
+        public MeterReadingUploadResult Result { get; set; }
+
+        public static MeterReadingUploadResponse Successful(MeterReadingUploadResult result)
+            => new MeterReadingUploadResponse() { Result = result, Success = true, Error = null };
+
+		public static MeterReadingUploadResponse Failure(string errorMessage)
+	        => new MeterReadingUploadResponse() { Result = null, Success = false, Error = errorMessage };
+    }
+}
